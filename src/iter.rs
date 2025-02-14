f632a6ed8117adb0c77ecec641944070206bd040
Sanpi <sanpi@homecomputing.fr>
2025-02-14 11:41:10 +0100

    Uses exdate for recur iter

--- src/iter.rs
+++ src/iter.rs
@@ -3,6 +3,8 @@ pub trait Recurring: Clone {
         None
     }
 
+    fn exdate(&self) -> &[crate::Date];
+
     fn set_dtstart(&mut self, _dtstart: crate::Date) {}
 
     fn dtend(&self) -> Option<&crate::Date> {
@@ -24,6 +26,10 @@ pub trait Recurring: Clone {
 impl Recurring for crate::VEvent {
     fn dtstart(&self) -> Option<&crate::Date> {
         Some(&self.dtstart)
+    }
+
+    fn exdate(&self) -> &[crate::Date] {
+        &self.exdate
     }
 
     fn set_dtstart(&mut self, dtstart: crate::Date) {
@@ -52,6 +58,10 @@ impl Recurring for crate::VJournal {
         Some(&self.dtstart)
     }
 
+    fn exdate(&self) -> &[crate::Date] {
+        &self.exdate
+    }
+
     fn set_dtstart(&mut self, dtstart: crate::Date) {
         self.dtstart = dtstart;
     }
@@ -68,6 +78,10 @@ impl Recurring for crate::VJournal {
 impl Recurring for crate::VTodo {
     fn dtstart(&self) -> Option<&crate::Date> {
         self.dtstart.as_ref()
+    }
+
+    fn exdate(&self) -> &[crate::Date] {
+        &self.exdate
     }
 
     fn set_dtstart(&mut self, dtstart: crate::Date) {
@@ -123,28 +137,38 @@ impl<T: Recurring> Iterator for Recur<T> {
 
         let mut next = self.item.clone();
 
-        let dtstart = next.dtstart()?;
-        let mut rrule = next.rrule()?.clone();
+        loop {
+            let dtstart = next.dtstart()?;
+            let mut rrule = next.rrule()?.clone();
 
-        if let Some(until) = &rrule.until {
-            if dtstart.date_naive() > until.date_naive() {
-                return None;
+            if let Some(until) = &rrule.until {
+                if dtstart.date_naive() > until.date_naive() {
+                    return None;
+                }
             }
+
+            let dtstart = rrule.clone() + *dtstart;
+            next.set_dtstart(dtstart);
+
+            if let Some(dtend) = next.dtend() {
+                let dtend = rrule.clone() + *dtend;
+                next.set_dtend(dtend);
+            }
+
+            next.set_rrule(rrule.clone());
+
+            if next.exdate().contains(&dtstart) {
+                continue;
+            }
+
+            if let Some(count) = rrule.count.as_mut() {
+                *count = count.checked_sub(1)?;
+                next.set_rrule(rrule.clone());
+            }
+
+            break;
         }
 
-        if let Some(count) = rrule.count.as_mut() {
-            *count = count.checked_sub(1)?;
-        }
-
-        let dtstart = rrule.clone() + *dtstart;
-        next.set_dtstart(dtstart);
-
-        if let Some(dtend) = next.dtend() {
-            let dtend = rrule.clone() + *dtend;
-            next.set_dtend(dtend);
-        }
-
-        next.set_rrule(rrule);
         self.item = next;
 
         Some(current)
@@ -224,6 +248,24 @@ mod test {
         let events = event.recurrent().between(now, end);
 
         assert_eq!(events.count(), 2);
+    }
+
+    #[test]
+    fn exdate() {
+        let event = crate::vevent! {
+            dtstart: "20240101",
+            rrule: {
+                freq: Yearly,
+                interval: 1,
+                count: 10,
+            },
+            exdate: ["20250101"],
+        }
+        .unwrap();
+
+        let mut events = event.recurrent();
+
+        assert_eq!(events.nth(1).unwrap().dtstart, "20260101".parse().unwrap());
     }
 
     #[test]
