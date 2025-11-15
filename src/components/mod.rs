2b2791ed01f6487a7cb5c19ddd06347da4302635
Sanpi <sanpi@homecomputing.fr>
2025-11-15 14:13:37 +0100

    Adds Components enum

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -27,3 +27,41 @@ pub(crate) enum Component {
     Timezone(crate::VTimezone),
     Todo(crate::VTodo),
 }
+
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
+pub enum Components {
+    Alarm,
+    Event,
+    Freebusy,
+    Journal,
+    Timezone,
+    Todo,
+}
+
+impl std::fmt::Display for Components {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = match self {
+            Self::Alarm => "VALARM",
+            Self::Event => "VEVENT",
+            Self::Freebusy => "VFREEBUSY",
+            Self::Journal => "VJOURNAL",
+            Self::Timezone => "VTIMEZONE",
+            Self::Todo => "VTODO",
+        };
+
+        f.write_str(s)
+    }
+}
+
+impl From<Component> for Components {
+    fn from(value: Component) -> Self {
+        match value {
+            Component::Alarm(_) => Self::Alarm,
+            Component::Event(_) => Self::Event,
+            Component::Freebusy(_) => Self::Freebusy,
+            Component::Journal(_) => Self::Journal,
+            Component::Timezone(_) => Self::Timezone,
+            Component::Todo(_) => Self::Todo,
+        }
+    }
+}
